a6b812686396c5732fe70896a593bb218dc01926
Jane Alam Tusher <jaanealamtusher@gmail.com>
2021-03-28 20:16:11 +0600

    password regex fix

--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -1,5 +1,5 @@
 const checkValidPass = (password: string): boolean => {
-    const re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    const re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$/;
     return re.test(password);
 };
 
