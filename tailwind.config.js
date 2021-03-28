5f23d9a2473b2fe4a2775f9360cc41bd8796611c
Jane Alam Tusher <jaanealamtusher@gmail.com>
2021-03-28 16:31:44 +0600

    tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // tailwind.config.js
 const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/**/*.tsx'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
